e5035e62c4c2c1cf7a5e0d82f8378582d5ee24db
MariaStoyanova5025 <mstoanova0@gmail.com>
2018-02-15 23:58:09 +0200

    i dont have i profile

---
+++ js.js
@@ -0,0 +1,28 @@
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+function getInfoLog()
+{
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    window.location.href = "/profile.html";
+}
+
+function getInfoReg()
+{
+    var username = document.getElementById("username").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+    console.log("-" + username + "-" + password)
+    if (!validateEmail(email))
+    {
+        var element = document.createElement("h1").innerHTML = "ne staa";
+         
+    }
+    if (password.length < 5)
+    {
+        console.log("too little");
+    }
+    window.location.href = "/profile.html";
+}
