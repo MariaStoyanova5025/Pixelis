5e093fef6ffcb317f1979dfa1d9e81d2975f62f7
mimi5025 <mstoanova0@gmail.com>
2018-04-14 23:08:33 +0300

    downloading image in jpeg

--- canvas.js
+++ canvas.js
@@ -66,4 +66,51 @@ function rubber()
 {
     curr_col = "white";
 }
- 
+
+    function triggerDownload (imgURI) {
+        var evt = new MouseEvent('click', {
+          view: window,
+          bubbles: false,
+          cancelable: true
+        });
+      
+        var a = document.createElement('a');
+        a.setAttribute('download', 'MY_COOL_IMAGE.png');
+        a.setAttribute('href', imgURI);
+        a.setAttribute('target', '_blank');
+      
+        a.dispatchEvent(evt);
+      }
+
+
+
+function download()
+{    
+
+  var svg = document.getElementById('svg');  
+  
+  
+
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var data = (new XMLSerializer()).serializeToString(svg);
+    var DOMURL = window.URL || window.webkitURL || window;
+  
+    var img = new Image();
+    var svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+    var url = DOMURL.createObjectURL(svgBlob);
+  
+    img.onload = function () {
+        console.log("izvinete????");
+      ctx.drawImage(img, 0, 0);
+      DOMURL.revokeObjectURL(url);
+  
+      var imgURI = canvas
+          .toDataURL('image/png')
+          .replace('image/png', 'image/octet-stream');
+  
+      triggerDownload(imgURI);
+    };
+  
+    img.src = url;
+}
